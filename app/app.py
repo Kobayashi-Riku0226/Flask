2ee276dc41dd726407be50a897f9dfa5e04cf372
Kobayashi <kobayashi.riku@classmethod.jp>
2025-09-25 15:35:47 +0900

    修正

--- app/app.py
+++ app/app.py
@@ -5,18 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+        app.logger.info('default access')
         return "<p>Hello, World!</p>"
 
 @app.route("/stdout")
 def stdout_log():
         # stdoutにログを出力
-        print("test access", file=sys.stdout, flush=True)
+        app.logger.info('test stdout access')
         return "<p>Logged to stdout</p>"
 
 @app.route("/stderr")
 def stderr_log():
         # stderrにログを出力
-        print("test err access", file=sys.stderr, flush=True)
+        app.logger.error('test stderr access')
         return "<p>Logged to stderr</p>"
 
 if __name__ == "__main__":
