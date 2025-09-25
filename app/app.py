313b334f766b9418205c0a7b5d2a231db17c1e03
Kobayashi <kobayashi.riku@classmethod.jp>
2025-09-25 16:21:41 +0900

    修正

--- app/app.py
+++ app/app.py
@@ -7,17 +7,17 @@ app.logger.setLevel(logging.DEBUG)
 @app.route("/")
 def hello_world():
         app.logger.info('default access')
-        return "<p>Hello, World!</p>"
+        return "<p>Hello, World!</p>", 200
 
 @app.route("/info")
 def stdout_log():
         app.logger.info('test info access')
-        return "<p>Logged to info</p>"
+        return "<p>Logged to info</p>", 200
 
 @app.route("/err")
 def stderr_log():
         app.logger.error('test err access')
-        return "<p>Logged to err</p>"
+        return "<p>Logged to err</p>", 500
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=80, debug=True)
